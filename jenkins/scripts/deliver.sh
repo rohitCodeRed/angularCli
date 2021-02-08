344e071a4974c34e50fec12c2dbca3f73e4b5a55
rohitCodeRed <rohit06051993@gmail.com>
2021-02-08 15:49:14 +0530

    All jenkins related files are created

---
+++ jenkins/scripts/deliver.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+#Run the angular project on port 4200
+set -x
+ng serve &
+sleep 1
+echo $! > .pidfile
+set +x
+
+echo 'Now...'
+echo 'Visit http://localhost:4200 to see your angular CLi login page application in action.'
